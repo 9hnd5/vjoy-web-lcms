8b00266b859520d3161d84c717d866f15ea41a7e
phuctran <phuctran.ngh@gmail.com>
2023-02-01 13:47:18 +0700

    VW-50 CRUD for users with fake data

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,18 @@ const theme = {
     fontSize: 16,
   },
 };
+
+import userComponents from "modules/user/components";
+
+import fakeDataProvider from 'ra-data-fakerest';
+const dataProvider = fakeDataProvider({ users: [{ firstName: "Alpha", lastName: "Test", dateOfBirth: '1990-01-01', email: 'test@mail.com', gender: 'Male', createdAt: "2023.1.21 10:23:55" }] }, true);
+
 const App = () => {
-  return <Admin dataProvider={dataService} theme={theme}></Admin>;
+  return (
+    <Admin dataProvider={dataProvider} theme={theme}>
+      <Resource name="users" {...userComponents.user} />
+    </Admin>
+  );
 };
 
 export default App;
--- src/modules/user/components/UserList.tsx
+++ src/modules/user/components/UserList.tsx
@@ -1,14 +1,35 @@
-import { Datagrid, EmailField, List, TextField } from "react-admin";
+import {
+  Datagrid,
+  DateField,
+  CreateButton,
+  EmailField,
+  FilterButton,
+  List,
+  TextField,
+  TextInput,
+  TopToolbar,
+} from "react-admin";
+
+const postFilters = [<TextInput label="Search" key="1" source="q" alwaysOn />];
+
+const PostListActions = () => (
+  <TopToolbar>
+    <FilterButton filters={postFilters} />
+    <CreateButton />
+  </TopToolbar>
+);
+
 export const UserList = () => {
   return (
-    <List>
+    <List filters={postFilters} actions={<PostListActions />}>
       <Datagrid rowClick="edit">
         <TextField source="id" />
         <TextField source="firstName" />
         <TextField source="lastName" />
-        <TextField source="dateOfBirth" />
+        <DateField source="dateOfBirth" locales="en-GB" />
         <EmailField source="email" />
         <TextField source="gender" />
+        <DateField source="createdAt" locales="en-GB" showTime={true} />
       </Datagrid>
     </List>
   );
