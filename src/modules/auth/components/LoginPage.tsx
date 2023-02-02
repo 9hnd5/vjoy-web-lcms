78c262545192e3043139de2d44b468910d131bca
9hnd5 <huy.nguyendinh@vus-etsc.edu.vn>
2023-02-02 16:13:50 +0700

    Custom login page

---
+++ src/modules/auth/components/LoginPage.tsx
@@ -0,0 +1,29 @@
+import { LoginForm } from "modules/auth/components/LoginForm";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: radial-gradient(circle at 50% 14em, #313264 0%, #00023b 60%, #00023b 100%);
+`;
+
+const ContainerItem = styled.div`
+  position: absolute;
+  top: 150px;
+`;
+
+export const LoginPage = () => {
+  return (
+    <Container>
+      <ContainerItem>
+        <LoginForm />
+      </ContainerItem>
+    </Container>
+  );
+};
