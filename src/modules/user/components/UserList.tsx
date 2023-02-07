4ed5aab9418d800cbfd505c86d240dba0c2d3622
phuctran <phuctran.ngh@gmail.com>
2023-02-07 15:20:20 +0700

    VW-50 CRUD for menu users

--- src/modules/user/components/UserList.tsx
+++ src/modules/user/components/UserList.tsx
@@ -4,31 +4,38 @@ import {
   CreateButton,
   EmailField,
   FilterButton,
+  FunctionField,
   List,
   TextField,
   TextInput,
   TopToolbar,
+  SelectField,
 } from "react-admin";
 
-const postFilters = [<TextInput label="Search" key="1" source="q" alwaysOn />];
+const listFilters = [<TextInput label="Search" key="search" source="q" alwaysOn />];
 
-const PostListActions = () => (
+const ListActions = () => (
   <TopToolbar>
-    <FilterButton filters={postFilters} />
+    <FilterButton filters={listFilters} />
     <CreateButton />
   </TopToolbar>
 );
 
 export const UserList = () => {
   return (
-    <List filters={postFilters} actions={<PostListActions />}>
+    <List filters={listFilters} actions={<ListActions />}>
       <Datagrid rowClick="edit">
         <TextField source="id" />
-        <TextField source="firstName" />
-        <TextField source="lastName" />
-        <DateField source="dateOfBirth" locales="en-GB" />
-        <EmailField source="email" />
-        <TextField source="gender" />
+        <TextField source="firstname" />
+        <TextField source="lastname" />
+        <EmailField source="email"/>
+        <TextField source="phone" />
+        <SelectField source="status" choices={[
+          { id: 0, name: "NEW" },
+          { id: 1, name: "ACTIVATED" },
+          { id: 2, name: "DEACTIVED" },
+        ]} />
+        <FunctionField render={(record: any) => record.role.name} />
         <DateField source="createdAt" locales="en-GB" showTime={true} />
       </Datagrid>
     </List>
