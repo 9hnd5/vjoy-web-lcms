889d988674da74026a41ba1fbc33b2e73bed3677
Tran Nguyen Huu Phuc <phuctran@vus-etsc.edu.vn>
2023-03-27 10:18:54 +0700

    VW-153 Implement cannon, background theme

--- src/gms/index.tsx
+++ src/gms/index.tsx
@@ -1,6 +1,7 @@
 import { Container } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Route, Routes } from "react-router-dom";
+import { WordBalloon } from "./modules/word-balloon/WordBalloon";
 
 const theme = createTheme({ typography: { fontFamily: "SVN-Gotham", fontSize: 16 } });
 
@@ -9,7 +10,7 @@ export const Gms = () => {
     <ThemeProvider theme={theme}>
       <Container disableGutters maxWidth={false}>
         <Routes>
-          <Route path="/word-balloon" />
+          <Route path="/word-balloon" element={<WordBalloon />} />
         </Routes>
       </Container>
     </ThemeProvider>
